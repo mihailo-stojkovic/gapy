3b3b1a3c46996fe62e1768fa1ec04e06f54bda3f
mihailo-stojkovic <mihailostojkovicpkr@gmail.com>
2021-05-06 15:03:49 +0200

    prototyping, added .gitignore for dummy.html

--- generator.py
+++ generator.py
@@ -9,7 +9,7 @@ class Generator:
         self._content = ""
         self._name = name_of_file
 
-
+    #Protected method which adds elements at the end of the self._content
     #The method is protected because later every single generator
     #will only have a certain number of elements
     def _add(self, element):
@@ -25,6 +25,22 @@ class Generator:
 
 
 
+    #protected method which adds content to the self._content
+    #before the tag (if option == 'b') or after the tag (if option =='a') 
+    def _add_content(self, tag, content, option = 'a'):
+        
+        #used for determening if the content should be before
+        #or after the tag
+        offset = 0
+        
+        if option == 'a':
+            offset = len(tag)
+
+        pos = self._content.find(tag) + offset
+        
+        self._content = self._content[ : pos] + content + self._content[pos : ]
+
+
 class HTMLGenerator(Generator):
 
 
@@ -35,23 +51,43 @@ class HTMLGenerator(Generator):
     def __init__(self, name_of_file, css_file_path, js_file_path):
         super().__init__(name_of_file)
         
-        self._add("<html>\n<head>\n")
-        self._add('<link rel="stylesheet" type="text/css" href="' + css_file_path + '" />\n')
-        self._add('<script type="text/javascript" src="' + js_file_path + '"> </script>\n')
-        self._add("<title>\n</title>\n</head>\n<body>\n</body>\n</html>")
+        self._add("<html><head>")
+        self._add('<link rel="stylesheet" type="text/css" href="' + css_file_path + '" />')
+        self._add('<script type="text/javascript" src="' + js_file_path + '"> </script>')
+        self._add("<title></title></head><body></body></html>")
 
-
+    #Generates the html file
+    #BeautifulSoup4 module is used so that the output html file is
+    #easily readable (it will probably be excluded once the project is complete...
+    # for now, its good for debugging)
     def generate(self):
         f = open(self.name + ".html", "w")
-        f.write(self.content)
+        from bs4 import BeautifulSoup as bs
+        soup = bs(self.content, features="lxml")
+        f.write(soup.prettify())
         f.close()
         
 
-
+#A dummy HTML class which has all of the elements which the base class has
+# plus additional text in the body tag (Only used for debugging)
 class DummyHTMLGenerator(HTMLGenerator):
     
     def __init__(self, css_file_path = "default.css", js_file_path = "default.js"):
         super().__init__("dummy", css_file_path, js_file_path)
         pos = self.content.find("<body>") + 6
         self._content = self._content[ : pos] + "Dummy HTML file" + self._content[pos : ]
-    
+
+#Prototype of the Javascript Generator class    
+class JSGenerator(Generator):
+    pass
+
+
+#Prototype of the CSS Generator class
+class CSSGenerator(Generator):
+    pass
+
+#Used for debugging
+"""
+test = DummyHTMLGenerator()
+test.generate()
+"""
